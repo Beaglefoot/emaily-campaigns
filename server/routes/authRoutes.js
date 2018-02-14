d301227ead92377d3d9d922b37c1c13d230a5711
Beaglefoot <ch-e-st@yandex.ru>
2018-02-14 23:23:22 +0300

    Finish login and logout via cookies

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -9,4 +9,13 @@ module.exports = app => {
   );
 
   app.get('/auth/google/callback', passport.authenticate('google'));
+
+  app.get('/api/current_user', (req, res) => {
+    res.send(`current user: ${req.user}`);
+  });
+
+  app.get('/api/logout', (req, res) => {
+    req.logout(); // .logout() attached to req by passport.js
+    res.send(`user after logout: ${req.user}`);
+  });
 };
--- server/services/passport.js
+++ server/services/passport.js
@@ -5,6 +5,11 @@ const keys = require('../config/keys');
 
 const User = mongoose.model('users');
 
+passport.serializeUser((userModel, done) => done(null, userModel.id));
+passport.deserializeUser((id, done) => {
+  User.findById(id).then(user => done(null, user));
+});
+
 passport.use(
   new GoogleStrategy(
     {
@@ -13,9 +18,13 @@ passport.use(
       callbackURL: '/auth/google/callback'
     },
     (accessToken, refreshToken, profile, done) => {
-      User.findOne({ googleId: profile.id }).then(existingUser => {
-        if (!existingUser) new User({ googleId: profile.id }).save();
-      });
+      User.findOne({ googleId: profile.id })
+        .then(
+          existingUser =>
+            existingUser || new User({ googleId: profile.id }).save()
+        )
+        .then(user => done(null, user))
+        .catch(err => console.error(err));
     }
   )
 );
