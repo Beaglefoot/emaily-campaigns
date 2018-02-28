b25d5502618fe1a85a184a5dfdee0d830daaa9d7
Beaglefoot <ch-e-st@yandex.ru>
2018-02-28 23:10:23 +0300

    Reorganize project for client part

--- server/index.js
+++ server/index.js
@@ -22,5 +22,5 @@ app.use(passport.session());
 
 require('./routes/authRoutes')(app);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 7000;
 app.listen(PORT, () => console.log(`express is running on PORT ${PORT}...`));
