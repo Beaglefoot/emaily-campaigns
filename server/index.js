359239479acaf29f93fe65e1042df804794b8204
Beaglefoot <ch-e-st@yandex.ru>
2018-03-15 21:08:22 +0300

    Default routes for heroku deployment

--- server/index.js
+++ server/index.js
@@ -25,5 +25,14 @@ app.use(passport.session());
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build')); // Serve production assets
+  const path = require('path');
+  // Show index.html on unknown route
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 const PORT = process.env.PORT || 7000;
 app.listen(PORT, () => console.log(`express is running on PORT ${PORT}...`));
